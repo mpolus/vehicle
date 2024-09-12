26d0d7030b1e4e8e818b3c276ab7a13aabe24d10
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-09-12 13:55:48 -0400

    vehicle

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Vehicle myCar = new Vehicle("Ford", "Fusion");
+        System.out.println(myCar.make);
+        System.out.println(myCar.model);
+    }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Vehicule {
+public class Vehicle {
+
+    public String make;
+    public String model;
+
+    public Vehicle(String make, String model){
+        this.make = make;
+        this.model = model;
+
+    }
+     public String getMake(){
+        return make;
+     }
+
+     public String getModel(){
+        return model;
+     }
+
+     public void setMake(String make){
+        this.make = make;
+     }
+
+     public void setModel(String model){
+        this.model = model;
+     }
 }
